7130a25b3a8cde3e8021160037325f1a183b9b38
Nithin Mathew <nithin.mathew@sysfore.com>
2024-06-04 12:28:58 +0530

    Added a new component called Job

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <User name="Nithin" age={25} email="nithin@gmail.com" />
-      <User name="Jack" age={21} email="jack-sparrow@gmail.com" />
+      {/* <User name="Nithin" age={25} email="nithin@gmail.com" />
+      <User name="Jack" age={21} email="jack-sparrow@gmail.com" /> */}
+      <Job salary={95000} position="Senior SDE" company="Sisco" />
+      <Job salary={45000} position="Junior SDE" company="Sisco" />
     </div>
   );
 }
@@ -16,6 +18,16 @@ const User = (props) => {
       <h1>{props.name}</h1>
       <h2>{props.age}</h2>
       <h3>{props.email}</h3>
+    </div>
+  );
+};
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>Salary: {props.salary}</h1>
+      <h2>Position: {props.position}</h2>
+      <h3>Company: {props.company}</h3>
     </div>
   );
 };
