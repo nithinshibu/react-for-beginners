6f29f2db26e62f576b50142733a522b7110fdc25
Nithin Mathew <nithin.mathew@sysfore.com>
2024-06-04 17:07:28 +0530

    Ternary Operator usage in react applications

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import "./App.css";
 
 function App() {
+  const age = 19;
+  const isGreen = true;
+
   return (
     <div className="App">
-      {/* <User name="Nithin" age={25} email="nithin@gmail.com" />
-      <User name="Jack" age={21} email="jack-sparrow@gmail.com" /> */}
-      <Job salary={95000} position="Senior SDE" company="Sisco" />
-      <Job salary={45000} position="Junior SDE" company="Sisco" />
+      {age >= 18 ? <OverAge /> : <UnderAge />}
+      <h1 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR</h1>
     </div>
   );
 }
 
-//this is a react component
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h3>{props.email}</h3>
-    </div>
-  );
+const OverAge = () => {
+  return <h1>OVER AGE</h1>;
 };
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1>Salary: {props.salary}</h1>
-      <h2>Position: {props.position}</h2>
-      <h3>Company: {props.company}</h3>
-    </div>
-  );
+const UnderAge = () => {
+  return <h1>UNDER AGE</h1>;
 };
 
 export default App;
