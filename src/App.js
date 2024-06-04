171b334344f4d49b48c56ea69bb7459d0f9d63f9
Nithin Mathew <nithin.mathew@sysfore.com>
2024-06-04 12:18:10 +0530

    added first component called user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"> Hello World</div>;
+  return (
+    <div className="App">
+      <User />
+    </div>
+  );
 }
+
+//this is a react component
+const User = () => {
+  return (
+    <div>
+      <h1>Nithin</h1>
+      <h2>25</h2>
+      <h3>nithin@gmail.com</h3>
+    </div>
+  );
+};
 
 export default App;
